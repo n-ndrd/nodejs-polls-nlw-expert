9e688a430f4ce606768d273912fa9795315bbf18
n-ndrd <ncamilada15@gmail.com>
2024-02-21 22:57:54 -0300

    Votos na enquete

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { randomUUID } from "node:crypto";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (req, res) => {
@@ -16,9 +17,52 @@ export async function voteOnPoll(app: FastifyInstance) {
         const { pollId } = voteOnPollParams.parse(req.params)
         const { pollOptionId } = voteOnPollBody.parse(req.body)
 
-        const sessionId = randomUUID()
+        let { sessionId } = req.cookies
 
+        if(sessionId){
+            const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+                where: {
+                    sessionId_pollId:{
+                        sessionId,
+                        pollId,
+                    }
+                }
+            })
 
+            if(userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+                await prisma.vote.delete({
+                    where:{
+                        id: userPreviousVoteOnPoll.id
+                    }
+                })
+
+                await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+            }
+            else if (userPreviousVoteOnPoll) {
+                return res.status(400).send({ message: 'You already voted on this poll.' })
+            }
+        }
+
+        if (!sessionId) {
+            sessionId = randomUUID()
+
+            res.setCookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                signed: true,
+                httpOnly: true,
+            })
+        }
+
+        await prisma.vote.create({
+            data: {
+                sessionId,
+                pollId,
+                pollOptionId,
+            }
+        })
+
+        await redis.zincrby(pollId, 1, pollOptionId)
 
         return res.status(201).send()
     })
