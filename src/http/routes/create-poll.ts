47718fafe6e4c39f136b069dcfaaf0326873d83d
n-ndrd <ncamilada15@gmail.com>
2024-02-22 23:16:31 -0300

    Adicionando o biomejs

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -1,28 +1,28 @@
-import { z } from "zod"
-import { prisma } from "../../lib/prisma"
-import { FastifyInstance } from "fastify"
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../../lib/prisma";
 
 export async function createPoll(app: FastifyInstance) {
-    app.post('/polls', async (req, res) => {
-        const createPollBody = z.object({
-            title: z.string(),
-            options: z.array(z.string())
-        })
+	app.post("/polls", async (req, res) => {
+		const createPollBody = z.object({
+			title: z.string(),
+			options: z.array(z.string()),
+		});
 
-        const { title, options } = createPollBody.parse(req.body)
+		const { title, options } = createPollBody.parse(req.body);
 
-        const poll = await prisma.poll.create({
-            data: {
-                title,
-                options: {
-                    createMany: {
-                        data: options.map(item => {
-                            return { title: item }
-                        }),
-                    }
-                }
-            }
-        })
-        return res.status(201).send({ pollId: poll.id })
-    })
+		const poll = await prisma.poll.create({
+			data: {
+				title,
+				options: {
+					createMany: {
+						data: options.map((item) => {
+							return { title: item };
+						}),
+					},
+				},
+			},
+		});
+		return res.status(201).send({ pollId: poll.id });
+	});
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,27 +1,26 @@
-import fastify from 'fastify';
-import cookie from '@fastify/cookie';
-import fastifyWebsocket from '@fastify/websocket';
+import cookie from "@fastify/cookie";
+import fastifyWebsocket from "@fastify/websocket";
+import fastify from "fastify";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
 
-import { voteOnPoll } from './routes/vote-on-poll';
-import { createPoll } from './routes/create-poll';
-import { pollResults } from './ws/poll-results';
-import { getPoll } from './routes/get-poll';
-
-const app = fastify()
+const app = fastify();
 
 app.register(cookie, {
-    secret: "poll-app-nlw", 
-    hook: 'onRequest', 
-    parseOptions: {} 
-})
+	secret: "poll-app-nlw",
+	hook: "onRequest",
+	parseOptions: {},
+});
 
-app.register(fastifyWebsocket)
+app.register(fastifyWebsocket);
 
-app.register(createPoll)
-app.register(getPoll)
-app.register(voteOnPoll)
-app.register(pollResults)
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
-    console.log('HTTP server running')
-})
+	console.log("HTTP server running");
+});
